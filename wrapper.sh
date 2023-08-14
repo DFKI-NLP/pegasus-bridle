700e45d838b933c63b29a5e846d2b6e88477f6db
Arne Binder <arne.binder@dfki.de>
2023-08-14 18:54:27 +0200

    prefix log message with INFO/WARNING

--- wrapper.sh
+++ wrapper.sh
@@ -7,10 +7,10 @@ SCRIPT_DIR=$(dirname $0)
 # check if an environment file is provided in the current directory
 ENV_FILE="$(pwd)/.pegasus-bridle.env"
 if [ -f "$ENV_FILE" ]; then
-    echo ".pegasus-bridle.env file found in current directory. load environment variables from $ENV_FILE"
+    echo "INFO: .pegasus-bridle.env file found in current directory. load environment variables from $ENV_FILE"
 else
     ENV_FILE="$SCRIPT_DIR"/.env
-    echo "no .pegasus-bridle.env file found in current directory. use environment variables from $ENV_FILE"
+    echo "WARNING: no .pegasus-bridle.env file found in current directory. use environment variables from $ENV_FILE"
 fi
 
 # import and check required environment variables

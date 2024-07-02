d2a45d6e7bca6480386301319343ec5154908820
Leonhard Hennig <leonhard.hennig@dfki.de>
2024-07-02 10:12:02 +0200

    updated conda install

--- activate_and_execute.sh
+++ activate_and_execute.sh
@@ -18,10 +18,9 @@ if ! command -v conda > /dev/null 2>&1; then
     gpg --keyring /usr/share/keyrings/conda-archive-keyring.gpg --no-default-keyring --fingerprint 34161F5BF5EB1D4BFBBB8F0A8AEB4F8B29D82806
     # Add our Debian repo
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/conda-archive-keyring.gpg] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" > /etc/apt/sources.list.d/conda.list
-    apt update
-    apt install conda
+    apt -qy update
+    apt -qy install conda
     source /opt/conda/etc/profile.d/conda.sh
-    conda -V
     echo "channels:" > /opt/conda/.condarc
     echo "  - conda-forge" >> /opt/conda/.condarc
     echo "channel_priority: strict" >> /opt/conda/.condarc
